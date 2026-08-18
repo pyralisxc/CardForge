e8bd8c5662a1404c24da36459312fd4dc204468e
Pyralis Cameron <49708731+pyralisxc@users.noreply.github.com>
2026-08-18 08:52:41 -0700

    Expose Studio media base64 decoder

--- src/features/studio-media/server.ts
+++ src/features/studio-media/server.ts
@@ -1,5 +1,6 @@
 export * from './model';
 export * from './server/StudioMediaError';
 export * from './server/studioMediaAccess';
+export * from './server/studioMediaBase64';
 export * from './server/studioMediaImage';
 export * from './server/studioMediaStore';
