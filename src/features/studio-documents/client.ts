863f672e081032d24f968028060ee2cd681fb7ac
Pyralis Cameron <49708731+pyralisxc@users.noreply.github.com>
2026-08-22 14:25:41 -0700

    Expose Set preview through Studio documents client interface

--- src/features/studio-documents/client.ts
+++ src/features/studio-documents/client.ts
@@ -1,5 +1,6 @@
 export * from './model';
 export { TemplateDraftPreviewClient } from './components/TemplateDraftPreviewClient';
+export { CardSetDraftPreviewClient } from './components/CardSetDraftPreviewClient';
 export { useStudioDocumentHandoff } from './hooks/useStudioDocumentHandoff';
 export { hydrateStudioDocumentAssets, hydrateStudioDocumentAssetValue } from './client/studioDocumentAssetHydration';
 export * from './assetReferences';
