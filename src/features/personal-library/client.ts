5969cca41b96e8bc6d7363d822875e33ed28c0e6
Pyralis Cameron <49708731+pyralisxc@users.noreply.github.com>
2026-08-23 09:58:44 -0700

    Expose connected personal library client contract

---
+++ src/features/personal-library/client.ts
@@ -0,0 +1,32 @@
+export {
+  chooseGoogleDrivePersonalLibraryItems,
+  loadPersonalLibrary,
+  materializePersonalLibraryItemContent,
+  registerGoogleDrivePersonalLibraryItems,
+  removePersonalLibraryItem,
+} from './client/personalLibraryClient';
+export type { PersonalLibraryContent } from './client/personalLibraryClient';
+export {
+  getPersonalLibraryRoleLabel,
+  isPersonalLibraryFontMimeType,
+  isPersonalLibraryImageMimeType,
+  isPersonalLibraryMimeTypeAllowedForRole,
+  isPersonalLibraryProvider,
+  isPersonalLibraryRole,
+  MAX_PERSONAL_LIBRARY_ITEM_BYTES,
+  MAX_PERSONAL_LIBRARY_ITEMS_PER_ACCOUNT,
+  MAX_PERSONAL_LIBRARY_REGISTER_BATCH,
+  PERSONAL_LIBRARY_FONT_MIME_TYPES,
+  PERSONAL_LIBRARY_IMAGE_MIME_TYPES,
+  PERSONAL_LIBRARY_PROVIDERS,
+  PERSONAL_LIBRARY_ROLES,
+} from './model';
+export type {
+  PersonalLibraryFontMimeType,
+  PersonalLibraryImageMimeType,
+  PersonalLibraryItem,
+  PersonalLibraryListResult,
+  PersonalLibraryProvider,
+  PersonalLibraryRegisterResult,
+  PersonalLibraryRole,
+} from './model';
