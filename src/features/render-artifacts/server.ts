538ae66f968d45c1bb477cbe58866c417f31a1c3
Pyralis Cameron <49708731+pyralisxc@users.noreply.github.com>
2026-08-22 21:26:45 -0700

    Expose canonical render artifact server interface

---
+++ src/features/render-artifacts/server.ts
@@ -0,0 +1,9 @@
+export { composeCanonicalContactSheet } from './server/contactSheet';
+export { renderCanonicalBrowserImages } from './server/canonicalBrowserRenderer';
+export {
+  getRenderArtifactId,
+  readRenderArtifact,
+  removeRenderArtifactsForStudioDocument,
+  RenderArtifactStoreError,
+  writeRenderArtifact,
+} from './server/renderArtifactStore';
