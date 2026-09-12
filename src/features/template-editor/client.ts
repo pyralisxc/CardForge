99a2930fe7940fad791fe9d2994350e7fc5c0726
Pyralis Cameron <49708731+pyralisxc@users.noreply.github.com>
2026-09-12 15:02:55 -0700

    Expose Template save impact through public client

--- src/features/template-editor/client.ts
+++ src/features/template-editor/client.ts
@@ -1,5 +1,6 @@
 export {
   prepareTemplateForLibrarySave,
   useTemplateLibraryActions,
+  type PendingTemplateSaveImpact,
 } from './hooks/useTemplateLibraryActions';
 export const loadCardTemplateMaker = () => import('./components/CardTemplateMaker');
